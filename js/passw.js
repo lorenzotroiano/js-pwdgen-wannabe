231387cb81d28d612154a046f1accb12ff0b5541
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-04-26 16:01:40 +0200

    completato esercizio js

---
+++ js/passw.js
@@ -0,0 +1,24 @@
+//  Chiedi all’utente il suo nome, 
+// poi chiedi il suo cognome,
+//  poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21 
+
+
+
+
+
+let nomeUtente = prompt("Inserisci il tuo nome");
+console.log(nomeUtente);
+
+
+
+let cognomeUtente = prompt("Inserisci il tuo cognome");
+console.log(cognomeUtente);
+
+
+
+let coloreUtente = prompt("Inserisci il tuo colore preferito");
+console.log(coloreUtente);
+
+
+document.getElementById("mio_id").innerHTML = nomeUtente  + " " + cognomeUtente + " " + coloreUtente + " " + 24;
